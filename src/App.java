1fa79c9fd75ecc1f56b7277421b8986641cbb3be
dantbm1 <dantbmps4@gmail.com>
2025-08-22 19:26:40 -0300

    primeiro desafio utilizando java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "daniel";
+        int xp = 9; 
+       /* 
+        try (Scanner scanner = new Scanner(System.in)){  
+            System.out.println("digite o nome do seu heroi");
+            nome = scanner.nextLine();
+            System.out.println("quanto xp voce tem(1-10)");
+            xp = scanner.nextInt();
+ 
+            */
+            if (xp < 1 ) { System.out.println("o heroi " + nome + " esta no nivel de Ferro");     
+            }
+            else if (xp <= 2 ) {System.out.println("o heroi " + nome + " esta no nivel de Broze");
+            }
+            else if (xp <= 5) {System.out.println("o heroi " + nome + " esta no nivel de Prata");
+                
+            } else if (xp <= 7 ) {System.out.println("o heroi " + nome + " esta no nivel de Ouro");
+                
+            }else if (xp <= 8) {System.out.println("o heroi " + nome + " esta no nivel de Platina");
+                
+            }else if (xp <= 9 ) {System.out.println("o heroi " + nome + " esta no nivel de Ascendete");
+                
+            }else if (xp <= 10 ) {System.out.println("o heroi " + nome + " esta no nivel de imortal");
+                
+            }else {System.out.println("o heroi " + nome + "esta no nivel de radiante ");
+                
+            }
+        }
+        
     }
-}
+
